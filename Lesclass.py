02ed8fb9e08ef92ee857b42908d2f6acbf7d4192
Your Name <you@example.com>
2021-05-04 19:40:01 +0600

    initional commit

---
+++ Lesclass.py
@@ -0,0 +1,19 @@
+
+class watch:
+
+    name = "Chronograph"
+    make = "Hamilton"
+    model = 2021
+
+    def start(self):
+        print("Tachymeter")
+
+    def stop(self):
+        print("Switzerland")
+
+
+watch_a = watch()
+
+watch_b = watch()
+
+print(watch_a.make, watch_b.stop())
